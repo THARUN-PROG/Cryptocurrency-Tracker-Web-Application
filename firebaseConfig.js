25abdcacdf867acd33706cd7ce389433d258b256
THARUN S <81684759+THARUN-PROG@users.noreply.github.com>
2023-10-29 20:32:05 +0530

    Add files via upload

---
+++ firebaseConfig.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCQfhyWOMM_LI4DtSL0cZCsMJGUJ8VtXZQ",
+    authDomain: "crypto-hunter-65b27.firebaseapp.com",
+    projectId: "crypto-hunter-65b27",
+    storageBucket: "crypto-hunter-65b27.appspot.com",
+    messagingSenderId: "747250508581",
+    appId: "1:747250508581:web:0dc4fb6237b2bdfbd770cc"
+  };
+
+
+export default firebaseConfig;
